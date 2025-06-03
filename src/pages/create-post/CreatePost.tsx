988f90d7c11d79b169517a15325d3b03fa8c99db
lorenzo-ap <ap.lorenzo03@gmail.com>
2025-06-03 16:26:17 +0300

    feat: set default value for dimension select

--- src/pages/create-post/CreatePost.tsx
+++ src/pages/create-post/CreatePost.tsx
@@ -26,7 +26,7 @@ export const CreatePostPage = () => {
     mode: 'uncontrolled',
     initialValues: {
       prompt: '',
-      size: '',
+      size: '1024x1024',
       generatedImage: {
         prompt: '',
         photo: ''
