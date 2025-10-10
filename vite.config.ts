76276a040f67ea49471db9c591398bf862bfacde
lorenzo-ap <ap.lorenzo03@gmail.com>
2025-10-11 00:20:51 +0300

    chore: removed env variables from vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,5 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-    'process.env.VITE_X_RAPIDAPI_KEY': JSON.stringify(process.env.VITE_X_RAPIDAPI_KEY)
-  }
+  plugins: [react()]
 });
